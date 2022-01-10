62bc5afab55e3d9a27fbc63794eaaffe31e73604
Kythosyer <dryanhood@gmail.com>
2022-01-10 20:31:38 +0200

    Final Configuration Commit

--- src/swuser.js
+++ src/swuser.js
@@ -1,6 +1,6 @@
 const sw = require('snoowrap');
 const swuser = new sw({
-    userAgent: 'Windows:scam-bot-finder:2.0.0 (by u/kythosyer)',
+    userAgent: 'Windows:scam-bot-finder:3.0.0 (by u/kythosyer)',
     clientId: 'Vs1J05bkHa1lgxNHJkvtsg',
     clientSecret: process.env.CLIENTSECRET,
     username: process.env.USERNAME,
